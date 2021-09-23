4c99516751cfec115d91dbcfe381d115a1513ccc
ludov <ludovic.cheron68@orange.fr>
2021-09-23 15:57:59 +0200

    feat: Side menu creation

--- app/src/main/java/fr/iut/monpotager/controller/MainActivity.java
+++ app/src/main/java/fr/iut/monpotager/controller/MainActivity.java
@@ -1,27 +1,142 @@
 package fr.iut.monpotager.controller;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.WindowManager;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.yarolegovich.slidingrootnav.SlidingRootNav;
+import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
+
+import java.util.Arrays;
 
 import fr.iut.monpotager.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DrawerAdapter.OnItemSelectedListener{
 
     /**
      * Create app
      * @param savedInstanceState
      */
+    private static final int POS_CLOSE = 0;
+    private static final int POS_DASHBOARD = 1;
+    private static final int POS_MY_PROFILE = 2;
+    private static final int POS_NEARBY_RES = 3;
+    private static final int POS_SETTINGS= 4;
+    private static final int POS_ABOUT_US = 5;
+    private static final int POS_LOGOUT = 7;
+
+    private String[] screenTitles;
+    private Drawable[] screenIcons;
+
+    private SlidingRootNav slidingRootNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FirebaseUser firebaseAuth = FirebaseAuth.getInstance().getCurrentUser();
-        Log.d("Firebase", "Is user anonymous:" + firebaseAuth.isAnonymous());
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        slidingRootNav = new SlidingRootNavBuilder(this)
+                .withDragDistance(180)
+                .withRootViewScale(0.75f)
+                .withRootViewElevation(25)
+                .withToolbarMenuToggle(toolbar)
+                .withMenuOpened(false)
+                .withContentClickableWhenMenuOpened(false)
+                .withSavedState(savedInstanceState)
+                .inject();
+
+        screenIcons = loadScreenIcons();
+        screenTitles = loadScreenTitles();
+
+        DrawerAdapter adapter = new DrawerAdapter(Arrays.asList(
+                createItemFor(POS_CLOSE),
+                createItemFor(POS_DASHBOARD).setChecked(true),
+                createItemFor(POS_MY_PROFILE),
+                createItemFor(POS_NEARBY_RES),
+                createItemFor(POS_SETTINGS),
+                createItemFor(POS_ABOUT_US),
+                new SpaceItem(260),
+                createItemFor(POS_LOGOUT)
+        ));
+        adapter.setListener(this);
+        RecyclerView list = findViewById(R.id.drawer_list);
+        list.setNestedScrollingEnabled(false);
+        list.setLayoutManager(new LinearLayoutManager(this));
+        list.setAdapter(adapter);
+
+        adapter.setSelected(POS_DASHBOARD);
+
     }
+    private DrawerItem createItemFor(int position){
+        return new SimpleItem(screenIcons[position], screenTitles[position])
+                .withIconTint(color(R.color.purple_200))
+                .withTextint(color(R.color.black))
+                .withSelectedIconTint(color(R.color.purple_200))
+                .withSelectedTextTint(color(R.color.purple_200));
+    }
+
+    @ColorInt
+    private int color(@ColorRes int res){
+        return ContextCompat.getColor(this, res);
+    }
+
+    private String[] loadScreenTitles() {
+        return getResources().getStringArray(R.array.id_activityScreenTitles);
+    }
+
+    private Drawable[] loadScreenIcons() {
+        TypedArray ta = getResources().obtainTypedArray(R.array.id_activityScreenTitles);
+        Drawable[] icons = new Drawable[ta.length()];
+        for(int i =0; i< ta.length(); i++){
+            int id = ta.getResourceId(i, 0);
+            if(id != 0){
+                icons[i] = ContextCompat.getDrawable(this, id);
+            }
+        }
+        ta.recycle();
+        return icons;
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    @Override
+    public void onItemSelected(int position) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        if (position == POS_DASHBOARD){
+            DashBoardFragment dashBoardFragment = new DashBoardFragment();
+            transaction.replace(R.id.container,dashBoardFragment);
+        }
+        else if (position == POS_MY_PROFILE){
+            ProfileFragment myProfileFragment = new ProfileFragment();
+            transaction.replace(R.id.container,myProfileFragment);
+        }
+        else if(position == POS_LOGOUT){
+            finish();
+        }
+        slidingRootNav.closeMenu();
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
 }
