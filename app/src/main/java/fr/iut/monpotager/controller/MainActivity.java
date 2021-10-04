dfa696e2eaadfb32d5b7190e8a343a22cc1617e8
Nehdi <yannaro35@gmail.com>
2021-10-04 22:43:27 +0200

    feat: Auth activities

--- app/src/main/java/fr/iut/monpotager/controller/MainActivity.java
+++ app/src/main/java/fr/iut/monpotager/controller/MainActivity.java
@@ -1,25 +1,34 @@
 package fr.iut.monpotager.controller;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.util.Log;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 import fr.iut.monpotager.R;
+import fr.iut.monpotager.controller.auth.LoginActivity;
+import fr.iut.monpotager.controller.auth.SignupActivity;
+import fr.iut.monpotager.manager.UserManager;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * Create app
-     * @param savedInstanceState
-     */
+    private UserManager userManager = UserManager.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (!userManager.isCurrentUserLogged()) {
+            //Toast.makeText(this, "test", Toast.LENGTH_LONG);
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        }
     }
 }
