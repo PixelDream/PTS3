99ffee68c6b5e25d6e1312be4428b067201f5351
Antoine Lancelot <alancelot27@gmail.com>
2021-10-12 11:24:52 +0200

    feat : home page design

--- app/src/main/java/fr/iut/monpotager/controller/MainActivity.java
+++ app/src/main/java/fr/iut/monpotager/controller/MainActivity.java
@@ -2,8 +2,10 @@ package fr.iut.monpotager.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.GridView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -17,9 +19,11 @@ public class MainActivity extends AppCompatActivity {
      * @param savedInstanceState
      */
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home);
     }
 }
