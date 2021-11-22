a80993a6ca7fdca5be50c7a75e187ced0ab8e97f
Morgan Nehdi <yannaro35@gmail.com>
2021-11-22 14:08:25 +0100

    fix: fix list layout

--- app/src/main/java/fr/iut/monpotager/controller/fragment/search/adapter/CustomVegetableListAdapter.java
+++ app/src/main/java/fr/iut/monpotager/controller/fragment/search/adapter/CustomVegetableListAdapter.java
@@ -65,10 +65,9 @@ public class CustomVegetableListAdapter extends BaseAdapter implements Filterabl
         TextView durationVegetable = view.findViewById(R.id.durationVegetable);
         int numOfWeeks = Math.round(vegetable.getDuration() / 7);
         durationVegetable.setText(numOfWeeks + " semaines");
-        final int radius = 50;
-        final int margin = 5;
-        final Transformation transformation = new RoundedCornersTransformation(radius, margin, RoundedCornersTransformation.CornerType.TOP);
-        Picasso.get().load(vegetable.getPicture()).transform(transformation).into(imageVegetable);
+
+        final Transformation transformation = new RoundedCornersTransformation(50, 5, RoundedCornersTransformation.CornerType.TOP);
+        Picasso.get().load(vegetable.getPicture()).transform(transformation).resize(2048,900).centerCrop().into(imageVegetable);
 
 
         return view;
